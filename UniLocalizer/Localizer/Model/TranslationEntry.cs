a7616a870290369cd4e9d3579b892135fb662e98
LukaszS <ls@xtech.pl>
2020-05-21 16:09:12 +0200

    JSON serialization cultureName => culture

--- UniLocalizer/Localizer/Model/TranslationEntry.cs
+++ UniLocalizer/Localizer/Model/TranslationEntry.cs
@@ -1,10 +1,12 @@
 ﻿namespace UniLocalizer.Localizer.Entity
 {
+    using Newtonsoft.Json;
     using System.ComponentModel.DataAnnotations.Schema;
 
     public abstract class TranslationEntry<T> where T : TranslationEntry<T>, new()
     {
         [Column(TypeName = "varchar(5)")]
+        [JsonProperty(PropertyName = "culture")]
         public string CultureName { get; set; }
 
     }
